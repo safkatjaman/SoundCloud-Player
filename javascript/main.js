e6b83acb472d4da30890a60d9bce1c58a32ae212
safkatjaman <safkatjamansiam20162@gmail.com>
2021-01-03 21:21:37 +0600

    Wrap Our SoundCloud Code in an Object

--- javascript/main.js
+++ javascript/main.js
@@ -3,17 +3,34 @@
 
 
 
-/* 2. Query SoundCloud API */
-SC.initialize({
-  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'artcell'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+
+
+/* 2. Query SoundCloud API */
+var SoundCloudAPI = {}; /* Using this object to wrap our code */ /* console.log(typeof(soundCloudAPI)); */
+
+
+
+/* API key initialization */
+SoundCloudAPI.init = function() {
+  SC.initialize({
+    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+  });
+}
+SoundCloudAPI.init(); /* calling the object (so called function) */
+
+
+
+/* getting the track */
+SoundCloudAPI.getTrack = function (inputValue) {
+  // find all sounds of buskers licensed under 'creative commons share alike'
+  SC.get('/tracks', {
+    q: inputValue
+  }).then(function(tracks) {
+    console.log(tracks);
+  });
+}
+SoundCloudAPI.getTrack('artcell'); /* calling the object (so called function :'3) */
 
 
 
