8d1a4b1e3de9e7904e660d99598e15de19b5e3d5
safkatjaman <safkatjamansiam20162@gmail.com>
2021-01-05 22:22:55 +0600

    Dynamically generate our HTML cards with JavaScript & Loop through our tracks with forEach()

--- javascript/main.js
+++ javascript/main.js
@@ -27,10 +27,90 @@ SoundCloudAPI.getTrack = function (inputValue) {
   SC.get('/tracks', {
     q: inputValue
   }).then(function(tracks) {
-    console.log(tracks);
+    /* console.log(tracks); */
+    SoundCloudAPI.renderTracks(tracks); /* calling the object (so called function :'3) */
   });
 }
-SoundCloudAPI.getTrack('artcell'); /* calling the object (so called function :'3) */
+SoundCloudAPI.getTrack('linkin park'); /* calling the object (so called function :'3) */
+
+
+
+SoundCloudAPI.renderTracks = function (tracks) {
+
+/* Making Cards Start */
+
+tracks.forEach(function (track) {
+
+console.log(track);
+/* Main Div Start */
+  var card = document.createElement('div');
+  card.classList.add('card');
+
+  var searchResults = document.querySelector('.js-search-results');
+/* Main Div End */
+
+
+/* Image Div Start */
+  var imageDiv = document.createElement('div');
+  imageDiv.classList.add('image');
+
+  var image = document.createElement('img');
+  image.classList.add('image-img');
+  image.setAttribute('id', 'image-css');
+  image.setAttribute('src', track.artwork_url || 'styles/themes/default/assets/images/SD Player logo.png');
+/* Image Div End */
+
+
+/* Header Div Start */
+  var contentDiv = document.createElement('div');
+  contentDiv.classList.add('content');
+
+  var headerDiv = document.createElement('div');
+  headerDiv.classList.add('header');
+
+  var linkHeader = document.createElement('a');
+  linkHeader.setAttribute('href', track.permalink_url);
+  linkHeader.setAttribute('target', '_blank');
+  linkHeader.innerHTML = track.title;
+/* Header Div End */
+
+
+/* Button Div Start */
+  var buttonDiv = document.createElement('div');
+  buttonDiv.classList.add('ui');
+  buttonDiv.classList.add('bottom');
+  buttonDiv.classList.add('attached');
+  buttonDiv.classList.add('button');
+  buttonDiv.classList.add('js-button');
+
+  var icon = document.createElement('i');
+  icon.classList.add('add');
+  icon.classList.add('icon');
+
+  var span = document.createElement('span');
+  span.innerHTML = "Add to playlist";
+/* Button Div End */
+
+
+/* Append Child Start */
+  card.appendChild(image);
+  card.appendChild(contentDiv);
+  card.appendChild(buttonDiv);
+
+  headerDiv.appendChild(linkHeader);
+
+  contentDiv.appendChild(headerDiv);
+
+  buttonDiv.appendChild(icon);
+  buttonDiv.appendChild(span);
+
+  searchResults.appendChild(card);
+/* Append Child End */
+
+});
+
+/* Make Cards End */
+}
 
 
 
